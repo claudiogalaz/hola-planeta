bc32c7c515803bed892c48955373db89f63361af
fpinab <fpinab@gmail.com>
2014-09-11 23:08:42 -0300

    ahora sí sin conflictos
    fomulario. falta cambiar el color de fondo y cambiar la posición del
    texto "ingrese comentario" (dejé ese campo estático por facilidad).

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@ $(document).ready(function(){
 		'margin-top' : -$box.height()/2 + 'px'
 	});
 
+	$box_form_contacto = $('#box_form_contacto');
+	$box_form_contacto.css({
+		'left' : '50%',
+		'top' : '50%',
+		'margin-left' : -$box_form_contacto.width()/2 + 'px',
+		'margin-top' : -$box_form_contacto.height()/2 + 'px'
+	});
+
 
 	//Subscription pup-up
 	$('.start_btn').click(function(){
@@ -19,11 +27,19 @@ $(document).ready(function(){
 		return false;
 	});
 
+	$('.contacto').click(function(){
+		$('#lightbox').width($(window).width()).height($(window).height()).fadeIn(200);
+		$('#box_form_contacto').fadeIn(200);
+
+		return false;
+	});
+
 	$('#lightbox, .close').click(function(){
 		$('#lightbox').width(0).height(0).fadeOut(200);
-		$('#box').fadeOut(200);
+		$('#box, #box_form_contacto').fadeOut(200);
 
 		return false;
 	});
 
 });
+
