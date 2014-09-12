bede151023ac7585a00382adfc03158668771187
fpinab <fpinab@gmail.com>
2014-09-12 11:25:43 -0300

    arreglo contacto + envío a correo

--- envio_correo.php
+++ envio_correo.php
@@ -2,13 +2,13 @@
 if(isset($_POST['email'])) {
 
 // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "proyectobluebox@gmail.com";
+$email_to = "therixsa@gmail.com";
 $email_subject = "Contacto desde el sitio web";
 
 // Aquí se deberían validar los datos ingresados por el usuario
 if(!isset($_POST['nombre']) ||
 !isset($_POST['email']) ||
-!isset($_POST['textarea'])) {
+!isset($_POST['mensaje'])) {
 
 echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
 echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
@@ -18,15 +18,15 @@ die();
 $email_message = "Detalles del formulario de contacto:\n\n";
 $email_message .= "Nombre: " . $_POST['nombre'] . "\n";
 $email_message .= "E-mail: " . $_POST['email'] . "\n";
-$email_message .= "Comentarios: " . $_POST['textarea'] . "\n\n";
+$email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
 
 
 // Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
+$headers = 'From: '. $_POST['email']."\r\n".
+'Reply-To: '.$_POST['email']."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-header( 'Location: index.html' );
-exit();
+@mail($_POST['email'], $email_subject, $email_message, $headers);
 }
+
+header ("Location: localhost");
 ?>
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
 
 	//Code to center the subscription pup-up box
+	/*
 	$box = $('#box');
 	$box.css({
 		'left' : '50%',
@@ -9,7 +10,7 @@ $(document).ready(function(){
 		'margin-left' : -$box.width()/2 + 'px',
 		'margin-top' : -$box.height()/2 + 'px'
 	});
-
+	*/
 	$box_form_contacto = $('#box_form_contacto');
 	$box_form_contacto.css({
 		'left' : '50%',
@@ -20,12 +21,14 @@ $(document).ready(function(){
 
 
 	//Subscription pup-up
+	/*
 	$('.start_btn').click(function(){
 		$('#lightbox').width($(window).width()).height($(window).height()).fadeIn(200);
 		$('#box').fadeIn(200);
 
 		return false;
 	});
+	*/
 
 	$('.contacto').click(function(){
 		$('#lightbox').width($(window).width()).height($(window).height()).fadeIn(200);
@@ -35,8 +38,8 @@ $(document).ready(function(){
 	});
 
 	$('#lightbox, .close').click(function(){
-		$('#lightbox').width(0).height(0).fadeOut(200);
-		$('#box, #box_form_contacto').fadeOut(200);
+		$('#lightbox').width(0).height(0).fadeOut(300);
+		$('#box_form_contacto').fadeOut(300);
 
 		return false;
 	});
