cf785fa72fc853fc9e5300203f8e01a63d5d6e21
fpinab <fpinab@gmail.com>
2014-10-31 22:44:38 -0300

    Contacto OK
    formulario ok a proyectobluebox@gmail.com
    
    Falta mandar mensaje de confirmación tras envío de correo

--- envio_correo.php
+++ envio_correo.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 
 // Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
-$email_to = "therixsa@gmail.com";
+$email_to = "proyectobluebox@gmail.com";
 $email_subject = "Contacto desde el sitio web";
 
 // Aquí se deberían validar los datos ingresados por el usuario
@@ -22,11 +22,11 @@ $email_message .= "Mensaje: " . $_POST['mensaje'] . "\n\n";
 
 
 // Ahora se envía el e-mail usando la función mail() de PHP
-$headers = 'From: '. $_POST['email']."\r\n".
+$headers = 'From: '. $_POST['nombre']."\r\n".
 'Reply-To: '.$_POST['email']."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($_POST['email'], $email_subject, $email_message, $headers);
+@mail($email_to, $email_subject, $email_message, $headers);
 }
 
-header ("Location: localhost");
+header ("Location: /");
 ?>
